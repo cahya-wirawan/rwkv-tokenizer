3b5a404518c23c342d0cc7dcf2a8e15cdb7340cb
Cahya Wirawan <cahya.wirawan@gmail.com>
2024-06-05 14:36:00 +0200

    replace division and modulo with bitwise shift and &

--- src/trie.rs
+++ src/trie.rs
@@ -12,9 +12,7 @@ impl TrieNode {
             id: 0
         };
         for index in 0..256 {
-            let index_a = index/16;
-            let index_b = index%16;
-            trinode.children[index_a as usize][index_b as usize] = None;
+            trinode.children[index >> 4][index & 15] = None;
         }
         trinode
     }
@@ -36,12 +34,13 @@ impl Trie {
     pub(crate) fn insert(&mut self, word: &Vec<u8>, id: u16) {
         let mut node = &mut self.root;
         for ch in word {
-            let index_a = u8::from_be(*ch)/16;
-            let index_b = u8::from_be(*ch)%16;
-            if node.children[index_a as usize][index_b as usize].is_none() {
-                node.children[index_a as usize][index_b as usize] = Option::from(Box::new(TrieNode::new()));
+            let ch = u8::from_be(*ch) as usize;
+            let index_a = ch >> 4;
+            let index_b = ch & 15;
+            if node.children[index_a][index_b].is_none() {
+                node.children[index_a][index_b] = Option::from(Box::new(TrieNode::new()));
             }
-            match &mut node.children[index_a as usize][index_b as usize] {
+            match &mut node.children[index_a][index_b] {
                 Some(next_node) => node = next_node,
                 None => unreachable!(),  // We've just checked that it's not None
             }
@@ -55,9 +54,10 @@ impl Trie {
         let mut index = 0;
         let mut old_index = 0;
         for ch in word {
-            let index_a = u8::from_be(*ch)/16;
-            let index_b = u8::from_be(*ch)%16;
-            if let Some(next_node) = &node.children[index_a as usize][index_b as usize]{
+            let ch = u8::from_be(*ch) as usize;
+            let index_a = ch >> 4;
+            let index_b = ch & 15;
+            if let Some(next_node) = &node.children[index_a][index_b]{
                 if node.id != 0 {
                     old_node = node;
                     old_index = index;
